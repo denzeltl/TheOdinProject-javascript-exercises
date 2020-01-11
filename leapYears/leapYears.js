c6771ddc9412f83fdcf31a4295cd5f02e7ac565f
Denzel <denzeltlee@gmail.com>
2020-01-11 23:03:10 +0800

    finished leapYear.js exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	console.log(year % 4 === 0);
+	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 module.exports = leapYears
