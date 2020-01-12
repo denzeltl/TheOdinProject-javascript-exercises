9a52ab2d0d10d398ff54c8d09890d62d1090732b
Denzel <denzeltlee@gmail.com>
2020-01-12 20:26:47 +0800

    finished tempConversion.js exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+	var deg = (degree - 32) * 5 / 9;
+	return Math.round(deg * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(degree) {
+	var deg = degree * 9 / 5 + 32;
+	return Math.round(deg * 10) / 10;
 }
 
 module.exports = {
